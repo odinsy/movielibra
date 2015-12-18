981c87e30ad9e08bb499a8b30e73f9d8de8a47be
odinsy <odidoit@gmail.com>
2015-12-18 23:40:12 +0300

    Hash converted to array

--- 03-movie.rb
+++ 03-movie.rb
@@ -14,33 +14,33 @@ else
   exit
 end
 
-hash = Hash.new {|key, value| key[value]={}} 
+arr = []
 
-movies.readlines.each do |movie|
+movies.map.each do |movie|
   movie.chomp!
   link, name, year, country, date, genre, duration, rating, director, actors = movie.split("|")
   actors = actors.split(",")
-  hash[name] = { link: link, year: year, country: country, date: date, genre: genre, duration: duration, rating: rating, director: director, actors: actors }
+  arr << {name: name, link: link, year: year, country: country, date: date, genre: genre, duration: duration, rating: rating, director: director, actors: actors}
 end   
 
 puts "5 longest movies"
-puts hash.sort_by { |k, v| v[:duration].gsub!(/ min/, '').to_i }.last(5)
+puts arr.sort_by { |v| v[:duration].gsub!(/ min/, '').to_i }.last(5)
 
 puts "\nComedy sorted by release date"
-puts hash.select { |k, v| v[:genre].include? "Comedy" }.sort_by {|k, v| v[:date] }
+puts arr.select { |v| v[:genre].include? "Comedy" }.sort_by {|v| v[:date] }
 
 puts "\nDirector list: "
-puts hash.map { |k, v| v[:director] }.sort.uniq
+puts arr.map { |v| v[:director] }.sort.uniq
 
 puts "\nDisplay count of not USA films"
-puts hash.reject { |k, v| v[:country] == "USA" }.count
+puts arr.reject { |v| v[:country] == "USA" }.count
 
 puts "\nDisplay count of films grouped by director"
-puts hash.group_by { |x, y| y[:director] }.map { |k, v| [k, v.count] }.sort
+puts arr.group_by { |y| y[:director] }.map { |k, v| [k, v.count] }.sort
 
 puts "\nDisplay count of films for every actor"
 h = Hash.new(0)
-puts hash.map { |x, y| y[:actors] }.flatten.inject(h) { |acc, n| acc[n] += 1; acc } 
+puts arr.map { |y| y[:actors] }.flatten.inject(h) { |acc, n| acc[n] += 1; acc }.sort_by {|k,v| v}.reverse
 
 
 
