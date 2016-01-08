f9cfd4f49f7d738d845cd2f6e95774b06a8129ea
odinsy <odidoit@gmail.com>
2016-01-08 16:31:24 +0300

    More metaprogramming.

--- lib/movie.rb
+++ lib/movie.rb
@@ -2,14 +2,18 @@
 
 require 'date'
 require './lib/rate.rb'
+require 'ostruct'
 
 class Movie
 
   include Rate
   attr_accessor :link, :name, :year, :country, :date, :genre, :duration, :rating, :director, :actors, :my_rating, :view_date
 
-  @@subclasses  = []
-  @@filters     = {}
+  @@filters = {}
+
+  class << self
+    attr_accessor :filters
+  end
 
   def initialize(list, attributes)
     @link, @name, @year, @country, @date, @genre, @duration, @rating, @director, @actors = attributes
@@ -24,18 +28,10 @@ class Movie
     @view_date  = nil
   end
 
-  class << self
-    attr_accessor :subclasses, :filters
-  end
-
-  def self.inherited(subclass)
-    @@subclasses << subclass
-  end
-
   def self.create(list, attrs)
-    @year = attrs[2].to_i
-    cls   = @@filters.detect { |cls, filter| filter.call }
-    cls.new(list, attrs)
+    @movie = OpenStruct.new(year: attrs[2].to_i)
+    cls = @@filters.detect { |cls, filter| @movie.instance_eval(&filter) }
+    cls[0].new(list, attrs)
   end
 
   def self.filter(&block)
@@ -44,7 +40,7 @@ class Movie
 
   class AncientMovie < Movie
     WEIGHT  = 30
-    filter { (1900..1944).cover?(@year) }
+    filter { (1900..1944).cover?(year) }
 
     def description
       "#{@name} — so old movie (#{@year} year)"
@@ -53,7 +49,7 @@ class Movie
 
   class ClassicMovie < Movie
     WEIGHT  = 50
-    filter { (1945..1967).cover?(@year) }
+    filter { (1945..1967).cover?(year) }
 
     def description
       "#{@name} — the classic movie. The director is #{@director}. Maybe you wanna see his other movies? \n#{@list.by_director(@director)}"
@@ -62,7 +58,7 @@ class Movie
 
   class ModernMovie < Movie
     WEIGHT  = 70
-    filter { (1968..1999).cover?(@year) }
+    filter { (1968..1999).cover?(year) }
 
     def description
       "#{@name} — modern movie. Starring: #{@actors}"
@@ -71,7 +67,7 @@ class Movie
 
   class NewMovie < Movie
     WEIGHT  = 100
-    filter { (2000..Date.today.year).cover?(@year) }
+    filter { (2000..Date.today.year).cover?(year) }
 
     def description
       "#{@name} — novelty!"
