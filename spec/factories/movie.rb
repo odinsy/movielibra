5b747bc35f603896a32d594a8e90d003b34cf752
odinsy <odidoit@gmail.com>
2016-02-28 00:02:07 +0300

    Added new factories; added fixes

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :movie do
-    skip_create
+    list { build(:movie_list) }
     attributes = ["http://www.imdb.com/title/tt0111161/", "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142", "9.3", "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton"]
     initialize_with { new("list", attributes) }
   end
--- spec/factories/my_movie_list.rb
+++ spec/factories/my_movie_list.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :movie_list do
+  factory :my_movie_list do
     movies = [
       ["http://www.imdb.com/title/tt0111161/", "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142", "9.3", "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton"],
       ["http://www.imdb.com/title/tt0068646/", "The Godfather", "1972", "USA", "1972-03-24", "Crime,Drama", "175", "9.2", "Francis Ford Coppola", "Marlon Brando,Al Pacino,James Caan"],
