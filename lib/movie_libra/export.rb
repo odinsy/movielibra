ebef3188b7bebb70b127205450551141afc3a15d
odinsy <odidoit@gmail.com>
2016-03-16 23:43:21 +0300

    added default path for saving

--- lib/movie_libra/export.rb
+++ lib/movie_libra/export.rb
@@ -2,11 +2,11 @@
 
 module Export
   # Export array with movie hashes to JSON
-  def save_to_json(filename)
+  def save_to_json(filename="./data/movies.json")
     File.open(filename, "w+") { |f| f.puts @list.to_json }
   end
   # Export array with movie hashes to CSV
-  def save_to_csv(filename)
+  def save_to_csv(filename="./data/movies.csv")
     CSV.open(filename, "w+", col_sep: "|") do |file|
       file << @list.first.keys
       @list.each do |m|
