544c8e62b3fd7b6598775b2e09a0e98a5da5758d
odinsy <odidoit@gmail.com>
2016-03-22 23:03:00 +0300

    add doc for class 'MyMovieList'

--- lib/movie_libra/my_movie_list.rb
+++ lib/movie_libra/my_movie_list.rb
@@ -3,14 +3,24 @@
 require 'movie_libra/rate_list.rb'
 
 module MovieLibra
+  # MyMovieList class
+  # @example
+  #   list = MyMovieLibra::MovieList.load_json('data/movies.json')
+  #   list = MyMovieLibra::MovieList.load_csv('data/movies.csv')
   class MyMovieList < MovieList
     include RateList
 
+    # Creates a new MyMovieLibra::MovieList object
+    # @param [Array] data   Array of the movie hashes
     def initialize(data)
       data    = [] unless data.is_a?(Array)
       @movies = data.map { |movie| Movie.create(self, movie) }
     end
 
+    # Finds movie by name
+    # @return [MovieLibra::Movie] movie information
+    # @example
+    #   list.find_movie("The Shawshank Redemption")
     def find_movie(name)
       @movies.detect { |movie| movie.name.casecmp(name.downcase).zero? }
     end
