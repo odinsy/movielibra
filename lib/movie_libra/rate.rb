34e9bac1a974b0f100cf96a09be4257c66c95e09
odinsy <odidoit@gmail.com>
2016-03-22 23:03:25 +0300

    add doc for the module 'Rate'

--- lib/movie_libra/rate.rb
+++ lib/movie_libra/rate.rb
@@ -2,8 +2,11 @@
 
 module MovieLibra
   class Movie
+    # Module which provides a functionality for rate watched movie
     module Rate
       # Rate and make viewed movie
+      # @param [Date] date    Date movie
+      # @param [Fixnum] num   Your movie rating
       def rate(date, num)
         unless (0..10).cover?(num)
           raise ArgumentError, 'You can rate movie only from 0 to 10!'
